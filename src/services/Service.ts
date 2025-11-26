4146eda3dc49e702a7bbaacedf4414c5f43f03a5
Milena <milenasvitras@gmail.com>
2025-11-26 12:54:05 -0300

    :Sparkles: Deploy do Projeto Blog Pessoal

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Instância do Axios
 const api = axios.create({
-    baseURL: 'https://blogpessoal-spring-1-zobg.onrender.com'
+    baseURL: import.meta.env.VITE_API_URL
     //baseURL: 'https://blogpessoal-security.onrender.com'
 })
 
