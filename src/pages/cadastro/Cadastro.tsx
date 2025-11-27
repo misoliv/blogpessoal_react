5310271f1f9017f4db61b52f45d4adeacefd05b3
Milena <milenasvitras@gmail.com>
2025-11-27 14:33:35 -0300

    :sparkles: modificação no cadastro

--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -33,7 +33,7 @@ function Cadastro() {
   }, [usuario])
 
   function retornar(){
-      navigate("/login");
+      navigate("/");
   }
 
   function atualizarEstado(e: ChangeEvent<HTMLInputElement>){
